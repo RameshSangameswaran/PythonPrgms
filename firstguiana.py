178a014772c6f57dacdfbc3c74917ae68f6a66b4
RameshSangameswaran <33337734+RameshSangameswaran@users.noreply.github.com>
2018-02-23 15:55:51 +0530

    Add files via upload

---
+++ firstguiana.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 23 15:50:41 2018
+
+@author: user
+"""
+
+#!/usr/bin/python
+
+import tkinter
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
