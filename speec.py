114b3a5595418719190a97581b13955ea93fa0f0
RameshSangameswaran <33337734+RameshSangameswaran@users.noreply.github.com>
2018-11-13 13:32:00 +0530

    Add files via upload

---
+++ speec.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.Microphone() as source:
+	r.adjust_for_ambient_noise(source,duration=5)
+	r.dynamic_energy_threshold = True
+	print("say something")
+	while True:
+		audio = r.listen(source)
+		print("you said " + r.recognize_google(audio))
